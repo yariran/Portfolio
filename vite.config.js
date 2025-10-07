32da589290f0ff11a160a72a7a2f229a4f98834a
Fatemeh Yaripour <yariran1380@gmail.com>
2025-10-07 22:21:00 +0330

    Add .nojekyll file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  }
 })
